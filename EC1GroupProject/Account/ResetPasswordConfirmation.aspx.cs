2a435c4f81405d3a85ae0ec2d4de39ed3f669cca
Kerette <kerette_111@yahoo.com>
2022-11-20 23:30:20 -0500

    Added Project

---
+++ EC1GroupProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EC1GroupProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EC1GroupProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EC1GroupProject.Startup))]
+namespace EC1GroupProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
